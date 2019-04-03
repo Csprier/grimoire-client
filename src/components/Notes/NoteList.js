89ec323818f3e5cecc72eee67b0100096a795f58
Cam Prier <CSPrier@gmail.com>
2019-04-03 17:04:45 -0400

    added some styles to the notes

--- src/components/Notes/NoteList.js
+++ src/components/Notes/NoteList.js
@@ -7,9 +7,11 @@ import RequiresLogin from '../requires-login';
 // Async actions
 import { getNotes } from '../../actions/notes.actions';
 
+// CSS
+import '../css/notes-list.css';
+
 class NoteList extends Component {
   componentDidMount() {
-    console.log('NoteList componentDidMount');
     this.props.dispatch(getNotes());
   }
 
@@ -19,11 +21,9 @@ class NoteList extends Component {
         <li className="note" key={note.id}>
           <h3>FI: {note.folderId} || {note.title}</h3>
           <p>{note.content}</p>
-          <ul>
+          <ul className="tags-container">
             <h4>Tags:</h4>
-            {note.tags.map(tag => {
-              return <li key={tag._id}>{tag.name}</li>
-            })}
+            {note.tags.map(tag => <li key={tag._id}>{tag.name}</li>)}
           </ul>
         </li>
       )
--- src/components/requires-login.js
+++ src/components/requires-login.js
@@ -5,7 +5,6 @@ import { Redirect } from 'react-router-dom';
 export default () => Component => {
 	function RequiresLogin(props) {
 		const { authenticating, loggedIn, error, ...passThroughProps } = props;
-		console.log('LI', loggedIn);
 		if (authenticating) {
 			return <div>Logging in...</div>;
     } 
