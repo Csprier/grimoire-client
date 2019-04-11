8acd72bbde2d58a9ec486ddbfd530f4dbcc7472c
Cam Prier <CSPrier@gmail.com>
2019-04-11 14:54:40 -0400

    modified some styles and added containers to jsx on notes

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// Async Actions
 import { deleteNoteById } from '../../actions/notes.actions';
+
+// CSS
+import '../css/note.css';
+import deleteIcon from '../../images/delete-button.png'
 
 class Note extends Component {
   handleDelete = (e) => {
@@ -14,14 +19,22 @@ class Note extends Component {
     return (
       <li className="note" key={key}>
         <h3>{title}</h3>
-        <button onClick={this.handleDelete} value={id}>Delete</button>
         <p>{content}</p>
-        <ul className="tags-container">
-          <h4>Tags:</h4>
-          {(tags.length > 0) 
-          ? tags.map(tag => <li key={tag._id}>{tag.name}</li>) 
-          : <p>No tags added yet</p>}
-        </ul>
+        <div className="note-information">
+          <ul className="tags-container">
+            <h4>Tags:</h4>
+            {(tags.length > 0) 
+            ? tags.map(tag => <li key={tag._id}>{tag.name}</li>) 
+            : <p>No tags added yet</p>}
+          </ul>
+          <button 
+            className="delete-button" 
+            onClick={this.handleDelete} 
+            value={id}
+          >
+            <img src={deleteIcon} alt="delete note" />
+          </button>
+        </div>
       </li>
     )
   }
