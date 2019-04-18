9b33e0d85a6e42398e303543f48268c5e13b6420
Cam Prier <CSPrier@gmail.com>
2019-04-18 13:57:11 -0400

    Added some styles to Tag component and cleaned up some rogue functions that were redundant

--- src/components/Tags/Tag.js
+++ src/components/Tags/Tag.js
@@ -16,7 +16,6 @@ class Tag extends Component {
   }
 
   render() {
-    console.log('tag t.p', this.props.tag);
     return (
       <li className="tag" key={this.props.tag.id}>
         <p>{this.props.tag.name}</p>
--- src/components/Tags/TagList.js
+++ src/components/Tags/TagList.js
@@ -8,7 +8,7 @@ import RequiresLogin from '../requires-login';
 import { getTags, deleteTagFromDatabase } from '../../actions/tags.actions'
 
 // Components
-// import Tag from './Tag';
+import Tag from './Tag';
 import AddTag from '../Tags/AddTag';
 
 
@@ -22,13 +22,6 @@ class TagList extends Component {
     this.props.history.push('/dashboard');
   }
 
-  handleDeleteTag = (e) => {
-    let tagId = e.target.value,
-        userId = this.props.userId;
-    console.log(`Delete: ${tagId} from database. UserId: ${userId}`);
-    this.props.dispatch(deleteTagFromDatabase(userId, tagId))
-  }
-
   render() {
     return (
       <div className="tag-list-container">
@@ -36,17 +29,8 @@ class TagList extends Component {
         <button onClick={this.returnToDashboard}>Back to Dashboard</button>
         <AddTag />
         { (this.props.tags !== undefined)
-          ? this.props.tags.map(tag => {
-              return (
-                <div className="tag" key={tag.id}>
-                  <p>{tag.name}</p>
-                  <button
-                    className="tag-delete-button"
-                    onClick={this.handleDeleteTag}
-                    value={tag.id}
-                  >X</button>
-                </div>)})
-          : this.props.tags.map(tag => <p>{tag.name}</p>)
+          ? this.props.tags.map(tag => <Tag tag={tag} key={tag.id} />)
+          : <span>No tags in the database</span>
         }
       </div>
     )
