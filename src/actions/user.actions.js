20928f08f840ae4eb0e1c71a3b141bde4092347e
Cam Prier <CSPrier@gmail.com>
2019-07-25 15:03:55 -0400

    Added CORS header to users.actions.js

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -6,6 +6,7 @@ export const createUser = (username, email, password) => dispatch => {
   return fetch(`${API_BASE_URL}/users`, {
     method: 'POST',
     headers: {
+      'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
